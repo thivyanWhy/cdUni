82f3e0f96d66324523bd0a3128979896959d4a71
thivyanWhy <thivyan.2014257@iit.ac.lk>
2015-03-15 12:24:43 +0530

    Print statment

--- src/cdUni/FirstClass.java
+++ src/cdUni/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String [] args){
 		System.out.println("No you suck");
 		System.out.println("Please God make this work");
+		System.out.println("Please let us have a shit together!!");
 	}
 
 }
