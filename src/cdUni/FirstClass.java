204d2facd3a5f6fe964c362ce37aabf0ee312b31
Savidude <mysticsavy@gmail.com>
2015-03-15 12:17:46 +0530

    Added one more line "Please God make this work"

--- src/cdUni/FirstClass.java
+++ src/cdUni/FirstClass.java
@@ -3,6 +3,7 @@ package cdUni;
 public class FirstClass {
 	public static void main(String [] args){
 		System.out.println("No you suck");
+		System.out.println("Please God make this work");
 	}
 
 }
