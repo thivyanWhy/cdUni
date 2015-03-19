d86552b5c7b6752994762d650b916d1569f26556
Savidude <mysticsavy@gmail.com>
2015-03-19 12:40:40 +0530

    Redesigned the main class

--- src/CD_Universe.java
+++ src/CD_Universe.java
@@ -9,29 +9,12 @@ public class CD_Universe
 	private static void primaryScreen() //This method shows the initial screen with the main set of options (add new item, edit items, delete items, rent items, overdue items)
 	{
 		
-		System.out.println("\t\t CD Universe"); //Title
-		System.out.println(); //Add space
-		
-		//Printing out the actions and their numbers
-		System.out.println("\t 0 - Add New Item");
-		System.out.println("\t 1 - Edit Item Details");
-		System.out.println("\t 2 - Delete Item");
-		System.out.println("\t 3 - Rent Item");
-		System.out.println("\t 4 - Overdue Items");
-		
-		System.out.print("Enter the required number to perform an action: "); // This is going to prompt the user to enter a number
-		action = input.nextInt();
-		System.out.println(); //Add line
-		
-		
-		
-		
-		//Giving an error message to an invalid input and making the user try again
-		while (!(action == 0 || action == 1 || action == 2 || action == 3 || action == 4))
-		{ 
-			System.err.println("Invalid input"); //Telling the user that this is an invalid input
+		do
+		{
+			System.out.println("\t\t CD Universe"); //Title
+			System.out.println(); //Add space
 			
-			//Telling the user to enter a valid input
+			//Printing out the actions and their numbers
 			System.out.println("\t 0 - Add New Item");
 			System.out.println("\t 1 - Edit Item Details");
 			System.out.println("\t 2 - Delete Item");
@@ -40,7 +23,14 @@ public class CD_Universe
 			
 			System.out.print("Enter the required number to perform an action: "); // This is going to prompt the user to enter a number
 			action = input.nextInt();
-		}
+			System.out.println(); //Add line
+			
+			//Giving an error to the user if he enters an invalid input
+			if (!(action == 0 || action == 1 || action == 2 || action == 3 || action == 4))
+			{
+				System.err.println("Invalid input");
+			}
+		}while (!(action == 0 || action == 1 || action == 2 || action == 3 || action == 4));
 	}
 	
 	private static void addNewItemActions() //This method executes the set of actions when the user wants to add a new item
@@ -61,27 +51,22 @@ public class CD_Universe
 			obj.setItemNumber(); //Prompts the user to set the item number
 			obj.showNewItemDetails(); //Giving a summary of the new item to the user
 			
-			//Giving the user the option to edit the item details if the given information is incorrect
-			System.out.print("Is this information correct? (Y/N) ");
-			informationState = input.next();
-			
-			
-			while (!((informationState.equalsIgnoreCase("Y") || (informationState.equalsIgnoreCase("N")))))//While loop B
-			//This statement exists if the user enters anything other than Y or N
+			do
 			{
-				//Giving the user the option to edit the item if the given information is incorrect
-				System.out.print("Is this information correct? (Y/N) ");
-				informationState = input.next();
-					
-				if(!((informationState.equalsIgnoreCase("Y") || informationState.equalsIgnoreCase("N"))))
-				{
-					System.err.println("Invalid input");
-					/*Prints "Invalid input" if the user enters anything other than Y or N
-					* Then the while loop B is carried out, asking the user if the information is correct
-					* If N is entered, the while loop A is carried out
-					*/
-				}
-			}
+				//This statement exists if the user enters anything other than Y or N
+					//Giving the user the option to edit the item if the given information is incorrect
+					System.out.print("Is this information correct? (Y/N) ");
+					informationState = input.next();
+						
+					if(!((informationState.equalsIgnoreCase("Y") || informationState.equalsIgnoreCase("N"))))
+					{
+						System.err.println("Invalid input");
+						/*Prints "Invalid input" if the user enters anything other than Y or N
+						* Then the while loop B is carried out, asking the user if the information is correct
+						* If N is entered, the while loop A is carried out
+						*/
+					}
+			}	while (!((informationState.equalsIgnoreCase("Y") || (informationState.equalsIgnoreCase("N")))));//While loop B	
 			
 		}
 	}
@@ -91,11 +76,12 @@ public class CD_Universe
 	{
 		primaryScreen();
 		
-			if (action == 0) 
-			{
-				addNewItemActions();
+		switch(action)
+		{
+			case 1:addNewItemActions();
 				primaryScreen(); //After a new item is added, the system should go back to the primary screen, without exiting the program
-			}
+				break;
+		}
 	}
 		
 
