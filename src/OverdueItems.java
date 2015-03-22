a2719af99d1c7fbfe6ab13ac2ad9164ef8d693de
Savidude <mysticsavy@gmail.com>
2015-03-22 14:05:53 +0530

    Made modifications to OverdueRentals class

--- src/OverdueItems.java
+++ src/OverdueItems.java
@@ -6,22 +6,47 @@ public class OverdueItems {
 
 	public void displayingOverdueItems()
 	{
-		System.out.println("Item Number \t\t Days borrowed \t\t Renter");
+		System.out.println("Item Number \t\t Name of Item \t\t Days borrowed \t\t RenterID \t\t Name of renter \t\t Telephone number");
 		
 		for (int i = 0; i < RentItem.rental.length; i++)
 		{
 			int daysBorrowedINT = 0;	
 			
-			if(RentItem.rental[i][4] != null)
+			if(RentItem.rental[i][4] != null) //If the borrowed time is not null
 			{
 				//Converting the days borrowed from integer to string
 				String daysBorrowed = RentItem.rental[i][4];
 				daysBorrowedINT = Integer.parseInt(daysBorrowed);
 			}
 			
+			String itemNumber = RentItem.rental[i][0];
+			String itemName = null;
+			String renterID = RentItem.rental[i][2];
+			String renterName = null;
+			String telNO = null;
+			
+			for(int x = 0; x<NewItem.itemArray.length; i++) //Finding the name of the rented item and renter, and telephone number of the renter
+			{
+				//Finding name of the rented item
+				if(NewItem.itemArray[x][3] != null && NewItem.itemArray[x][3].equals(itemNumber))
+				{
+					itemName = NewItem.itemArray[x][1];
+					break;
+				}
+				
+				//Finding name of renter and telephone number
+				if(addNewMember.newMemberArray[x][3] != null && addNewMember.newMemberArray[x][3].equals(renterID))
+				{
+					renterName = addNewMember.newMemberArray[x][0];
+					telNO = addNewMember.newMemberArray[x][2];
+					break;
+				}
+				
+			}
+			
 			if(RentItem.rental[i][1] != null && RentItem.rental[i][1].equals("Weekly") && daysBorrowedINT > 7) //Show if the list of rented items is of weekly type
 			{
-				System.out.println(RentItem.rental[i][0] + "\t\t\t" + RentItem.rental[i][4] + "\t\t\t" + RentItem.rental[i][2]);
+				System.out.println(itemNumber + "\t\t\t" + itemName + "\t\t\t" + daysBorrowedINT + "\t\t\t" + renterID + "\t\t\t" + renterName + "\t\t\t" + telNO);
 			}
 			
 			if(RentItem.rental[i][1] != null && RentItem.rental[i][1].equals("Overnight") && daysBorrowedINT > 1) //Show if the list of rented items is of overnight type
@@ -34,6 +59,4 @@ public class OverdueItems {
 		System.out.println("Press any key and hit enter to go back");
 		String next = in.next();
 	}
-
-	//Discuss additional functionalitites to this
 }
